b4e111ae490347161f0181b628c8090e1fb4167c
huangjingkun <1078156216@qq.com>
2024-11-11 13:59:31 +0800

    初始化

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
-        ? '/vuejs-essential/dist/'
+        ? '/vue-essential/dist/'
         : '/',
   chainWebpack: config =>{
     config.plugin('html')
